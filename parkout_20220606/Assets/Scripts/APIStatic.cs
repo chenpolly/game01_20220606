8cb7b79d760998afa13a0774b9a88314f2ba6dd7
polly <105775304+chenpolly@users.noreply.github.com>
2022-06-20 10:13:15 +0800

    update靜態API練習作業

--- parkout_20220606/Assets/Scripts/APIStatic.cs
+++ parkout_20220606/Assets/Scripts/APIStatic.cs
@@ -34,6 +34,24 @@ using UnityEngine;
             #region
             print("攝影機的數量"+ Camera.allCamerasCount);
             print("使用的應用平台:"+Application.platform);
+            print("睡眠臨界值：" + Physics.sleepThreshold);
+            Time.timeScale = 0.5f;
+            print("時間大小 :" + Time.timeScale);
+            //靜態方法，類別名稱.靜態方法(對應的引數)
+            int num = (int)Mathf.Round(9.999f);
+            print("9.999去除小數點四捨五入: " + num);
+            
+            
+            Vector3 myVector1;
+            Vector3 myVector2;
+            myVector1 = new Vector3(1, 1, 1);
+            myVector2 = new Vector3(22, 22, 22);
+            float dist = Vector3.Distance(myVector1, myVector2);
+            print("<color=yellow>兩點相距:"+dist+"</color>");
+
+            //開啟連結
+            Application.OpenURL("https://unity.com/");
+
             #endregion
         }
         private void Update()
@@ -43,14 +61,18 @@ using UnityEngine;
             int range = Random.Range(1, 3);
             print("隨機範圍：" + range);
             #endregion
+            
             //練習
             #region
-            if (Input.anyKey)            
-            {
-                print("enter any key");
-            }
-            print("你玩了多久了："+ Time.time);
+            //print("是否按下任意鍵："+ Input.anyKeyDown);
+            //print("遊戲經過時間："+ Time.timeSinceLevelLoad);
 
+            //靜態方法
+            print("<color=red>是否按下空白鍵：" + Input.GetKeyDown(KeyCode.Space) + "</color>");
+            
+       
+            
+            
             #endregion
         }
 
